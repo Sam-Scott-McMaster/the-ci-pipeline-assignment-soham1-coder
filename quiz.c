14c77f52fc819af27b3d68a9eff0248d7d84a21d
soham <sohamhajariwala@outlook.com>
2024-10-03 15:12:30 -0400

    second commit

--- quiz.c
+++ quiz.c
@@ -5,9 +5,9 @@
 int main(int argc, char *argv[]) {
 
 
-if (argc>2 && strcmp(argv[1],"-1") == 0){
+if (argc>3 && strcmp(argv[2],"-1") == 0 && strcmp(argv[1],"quiz") == 0){
 
-    if (strcmp(argv[2], "touch") == 0){
+    if (strcmp(argv[3], "touch") == 0){
         puts("correct");
         return EXIT_SUCCESS;
     }
@@ -19,9 +19,9 @@ if (argc>2 && strcmp(argv[1],"-1") == 0){
 
 }
 
-if (argc>2 && strcmp(argv[1],"-2") == 0){
+if (argc>3 && strcmp(argv[2],"-2" ) == 0 && strcmp(argv[1],"quiz") == 0){
 
-    if (strcmp(argv[2], "status") == 0){
+    if (strcmp(argv[3], "status") == 0){
         puts("correct");
         return EXIT_SUCCESS;
     }
@@ -32,9 +32,9 @@ if (argc>2 && strcmp(argv[1],"-2") == 0){
     }
 
 }
-if (argc>2 && strcmp(argv[1],"-3") == 0){
+if (argc>3 && strcmp(argv[2],"-3") == 0 && strcmp(argv[1],"quiz") == 0){
 
-    if (strcmp(argv[2], "printf") == 0){
+    if (strcmp(argv[3], "printf") == 0){
         puts("correct");
         return EXIT_SUCCESS;
     }
@@ -45,20 +45,24 @@ if (argc>2 && strcmp(argv[1],"-3") == 0){
     }
 
 }
+if (strcmp(argv[1],"quiz") == 0 && argc == 2) {
+puts("Usage: quiz [-#] [answer]");
+puts("Try 'quiz --help' for more information");
+}
 
-if (strcmp(argv[1],"-1") == 0 && argc<3) {
+if (strcmp(argv[2],"-1") == 0 && argc<4 && strcmp(argv[1],"quiz") == 0) {
 puts("question 1: What is the command to create a new file?");
 }
-if (strcmp(argv[1],"-2") == 0 && argc<3) {
+if (strcmp(argv[2],"-2") == 0 && argc<4 && strcmp(argv[1],"quiz") == 0) {
 puts("question 2: What is the command to check the current status of a git repository? (the word you enter after 'git')");
 }
 
-if (strcmp(argv[1],"-3") == 0 && argc<3) {
+if (strcmp(argv[2],"-3") == 0 && argc<4 && strcmp(argv[1],"quiz") == 0) {
 puts("question 3: What is the command to output formatted text in C");
 }
 
-if (strcmp(argv[1],"--help") == 0 && argc<3) {
-puts("heres some help: enter '-1' to view question and -1 'answer' to provide an answer to the question");
+if (strcmp(argv[2],"--help") == 0 && argc<4 && strcmp(argv[1],"quiz") == 0) {
+puts("enter 'quiz -1' to view question and quiz -1 'answer' to provide a one word answer to the question");
 }
 
 return EXIT_SUCCESS;
