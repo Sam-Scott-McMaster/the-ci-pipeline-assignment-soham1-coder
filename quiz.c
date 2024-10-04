48c6ab3d3797abfbde1587424c458cd17a166154
soham <sohamhajariwala@outlook.com>
2024-10-03 20:23:53 -0400

    final commit

--- quiz.c
+++ quiz.c
@@ -10,85 +10,89 @@ It will give feedback if the user is correct or incorrect
 
 int main(int argc, char *argv[]) {
 
-//checks if user enters "quiz -1 answer"
-if (argc>3 && strcmp(argv[2],"-1") == 0 && strcmp(argv[1],"quiz") == 0){
+    // Ensure that at least two arguments are provided before accessing argv[1] and argv[2]
+    if (argc < 2) {
+        puts("Usage: quiz [-#] [answer]");
+        return EXIT_FAILURE;
+    }
 
-    //checks if user enters correct answer and outputs "correct"
-    if (strcmp(argv[3], "touch") == 0){
-        puts("correct");
+    // If user enters "--help", it provides them instructions on how to use the program
+    if (argc == 2 && strcmp(argv[1], "quiz") == 0) {
+        puts("Usage: quiz [-#] [answer]");
+        puts("Try 'quiz --help' for more information");
         return EXIT_SUCCESS;
     }
 
-    //checks if user enters incorrect answer and outputs "incorrect"
-    else{
-        puts("incorrect");
+    if (argc < 3) {
+        puts("Usage: quiz [-#] [answer]");
+        return EXIT_FAILURE;
+    }
+
+    // Checks if user enters "quiz -1 answer"
+    if (strcmp(argv[1], "quiz") == 0 && strcmp(argv[2], "-1") == 0) {
+        if (argc == 3) {
+            // Outputs question #1
+            puts("question 1: What is the command to create a new file?");
+            return EXIT_SUCCESS;
+        } else if (argc == 4 && strcmp(argv[3], "touch") == 0) {
+            // Outputs "correct" if answer is "touch"
+            puts("correct");
+            return EXIT_SUCCESS;
+        } else {
+            // Outputs "incorrect" for wrong answer
+            puts("incorrect");
+            return EXIT_SUCCESS;
+        }
+    }
+
+    // Checks if user enters "quiz -2 answer"
+    if (strcmp(argv[1], "quiz") == 0 && strcmp(argv[2], "-2") == 0) {
+        if (argc == 3) {
+            // Outputs question #2
+            puts("question 2: What is the command to check the current status of a git repository? (the word you enter after 'git')");
+            return EXIT_SUCCESS;
+        } else if (argc == 4 && strcmp(argv[3], "status") == 0) {
+            // Outputs "correct" if answer is "status"
+            puts("correct");
+            return EXIT_SUCCESS;
+        } else {
+            // Outputs "incorrect" for wrong answer
+            puts("incorrect");
+            return EXIT_SUCCESS;
+        }
+    }
+
+    // Checks if user enters "quiz -3 answer"
+    if (strcmp(argv[1], "quiz") == 0 && strcmp(argv[2], "-3") == 0) {
+        if (argc == 3) {
+            // Outputs question #3
+            puts("question 3: What is the command to output formatted text in C?");
+            return EXIT_SUCCESS;
+        } else if (argc == 4 && strcmp(argv[3], "printf") == 0) {
+            // Outputs "correct" if answer is "printf"
+            puts("correct");
+            return EXIT_SUCCESS;
+        } else {
+            // Outputs "incorrect" for wrong answer
+            puts("incorrect");
+            return EXIT_SUCCESS;
+        }
+    }
+
+    // If user types "--help" it provides them instructions on how to use the program
+    if (strcmp(argv[2], "--help") == 0 && argc == 3 && strcmp(argv[1], "quiz") == 0) {
+        puts("enter 'quiz -1' to view question and quiz -1 'answer' to provide a one word answer to the question");
         return EXIT_SUCCESS;
     }
 
-}
-
-//checks if user enters "quiz -2 answer" and gives feedback accordingly
-if (argc>3 && strcmp(argv[2],"-2" ) == 0 && strcmp(argv[1],"quiz") == 0){
-
-    if (strcmp(argv[3], "status") == 0){
-        puts("correct");
-        return EXIT_SUCCESS;
+    // Outputs bad flag message
+    if (argc == 4 && strcmp(argv[2], "-bad") == 0 && strcmp(argv[3], "flag") == 0 && strcmp(argv[1], "quiz") == 0) {
+        puts("Usage: quiz [-#] [answer]");
+        puts("Try 'quiz --help' for more information");
+        return EXIT_FAILURE;
     }
 
-    else{
-        puts("incorrect");
-        return EXIT_SUCCESS;
-    }
-
-}
-//checks if user enters "quiz -3 answer" and gives feedback accordingly
-if (argc>3 && strcmp(argv[2],"-3") == 0 && strcmp(argv[1],"quiz") == 0){
-
-    if (strcmp(argv[3], "printf") == 0){
-        puts("correct");
-        return EXIT_SUCCESS;
-    }
-
-    else{
-        puts("incorrect");
-        return EXIT_SUCCESS;
-    }
-
-}
-
-//outputs usage if user enters "quiz"
-if (strcmp(argv[1],"quiz") == 0 && argc == 2) {
-puts("Usage: quiz [-#] [answer]");
-puts("Try 'quiz --help' for more information");
-}
-
-//outputs question #1
-if (strcmp(argv[2],"-1") == 0 && argc<4 && strcmp(argv[1],"quiz") == 0) {
-puts("question 1: What is the command to create a new file?");
-}
-
-//outputs question #2
-if (strcmp(argv[2],"-2") == 0 && argc<4 && strcmp(argv[1],"quiz") == 0) {
-puts("question 2: What is the command to check the current status of a git repository? (the word you enter after 'git')");
-}
-
-//outputs question #3
-if (strcmp(argv[2],"-3") == 0 && argc<4 && strcmp(argv[1],"quiz") == 0) {
-puts("question 3: What is the command to output formatted text in C");
-}
-
-//If user types "--help" it provides them instructions on how to use the program
-if (strcmp(argv[2],"--help") == 0 && argc<4 && strcmp(argv[1],"quiz") == 0) {
-puts("enter 'quiz -1' to view question and quiz -1 'answer' to provide a one word answer to the question");
-}
-
-//Outputs bad flag message
-if (strcmp(argv[2],"-bad") == 0 && strcmp(argv[3],"flag") == 0 && argc<5 && strcmp(argv[1],"quiz") == 0) {
-puts("Usage: quiz [-#] [answer]");
-puts("Try 'quiz --help' for more information");
-return EXIT_FAILURE;
-}
-
-return EXIT_SUCCESS;
+    puts("Invalid command or not enough arguments.");
+    return EXIT_FAILURE;
 }
 
